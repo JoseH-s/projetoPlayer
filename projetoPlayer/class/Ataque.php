529f17de32bb8093595ca488465d41c39efe4ac5
JoseH-s <josehribeirodossantos@gmail.com>
2024-11-27 17:18:48 -0300

    Add files via upload

---
+++ projetoPlayer/class/Ataque.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('Item.php');
+
+class Ataque extends Item{
+    public function __construct(string $nome, int $tamanho , string $classe) {
+        parent::__construct($nome, $tamanho, $classe );
+    }
+}
