522414e6d300afd30d95991fbe45e618e6fe4edd
JoseH-s <josehribeirodossantos@gmail.com>
2024-11-27 17:45:51 -0300

    Delete projetoPlayer directory

--- projetoPlayer/class/Ataque.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('Item.php');
-
-class Ataque extends Item{
-    public function __construct(string $nome, int $tamanho , string $classe) {
-        parent::__construct($nome, $tamanho, $classe );
-    }
-}
