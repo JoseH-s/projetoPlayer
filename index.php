2ae8eb9861a28d2c544f2ee77bbd339087363ce9
JoseH-s <josehribeirodossantos@gmail.com>
2024-11-27 20:17:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,29 +7,38 @@ require_once('./class/Magia.php');
 require_once('./class/Ataque.php');
 require_once('./class/Defesa.php');
 
-$magia = new Magia("Fogo", 2, "Magia");
-$ataque = new Ataque("Espada", 7, "Ataque");
-$ataque1 = new Ataque("Arco" , 7 , "Ataque");
-$ataque2 = new Ataque("Lamina" , 7 , "Ataque");
+$magia = new Magia("Fogo");
+$magia1 = new Magia("Gelo");
+
+$ataque = new Ataque("Espada");
+$ataque1 = new Ataque("Arco");
+
+$defesa = new Defesa("Escudo");
+$defesa1 = new Defesa("Armadura");
 
 
-
-$inventario = New Inventario();
-
-$player = new Player("Cleitinho", 1, $inventario);
+$player = new Player("Cleitinho");
 $player->subirNivel();
 $player->subirNivel();
 $player->subirNivel();
 $player->subirNivel();
 $player->coletarItem($magia);
-$player->coletarItem($magia);
-$player->coletarItem($magia);
+$player->coletarItem($magia1);
 $player->coletarItem($ataque);
-$player->soltarItem("Espada");
-$inventario->capacidadeLivre();
+$player->coletarItem($ataque1);
+$player->getInventario()->capacidadeLivre();
 
 
 
+$player1 = new Player("Renata");
+$player1->coletarItem($defesa);
+$player1->coletarItem($defesa1);
+$player1->coletarItem($ataque);
+$player1->coletarItem($ataque);
+$player1->coletarItem($ataque1);
+$player1->coletarItem($magia);
+$player1->coletarItem($magia1);
+$player1->getInventario()->capacidadeLivre();
 
 
 
